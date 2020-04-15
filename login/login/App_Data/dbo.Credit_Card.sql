9194bee9f64b9f1b91f99b95b09eb13f0a59c536
jmbeckman <59981233+jmbeckman@users.noreply.github.com>
2020-04-15 12:01:56 -0500

    Database tables update
    Update the names of several of the fields within each table in order to eliminate spaces.

---
+++ login/login/App_Data/dbo.Credit_Card.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Credit Card] (
+    [Email]              VARCHAR (50) NOT NULL,
+    [Cardnumber]        INT          NOT NULL,
+    [Securitycode]      INT          NOT NULL,
+    [Expirationdate]    DATETIME     NOT NULL,
+    [Cardholdersname] VARCHAR (50) NOT NULL,
+    [Username]           VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Email] ASC)
+);
+
