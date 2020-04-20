d03c295ef9faf054100da3d0b2388c19e76fd39f
jmbeckman <59981233+jmbeckman@users.noreply.github.com>
2020-04-20 12:14:56 -0500

    ORM database
    Added in a class to start to implement ORM into the database in order to improve the performance of the database.

---
+++ login/login/Database.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace login
+{
+    public class Database : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Creditcard> Creditcards { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlite("Data Source=database.db");
+    }
+    public class User
+    {
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Birthdate { get; set; }
+
+    }
+    public class creditcard
+    {
+        public string Email { get; set; }
+        public string Expirationdate { get; set; }
+        public string Cardname { get; set; }
+        public string Username{ get; set; }
+        public int Cardnumber { get; set; }
+        public int Code { get; set; }
+    }
+    public class Bank
+    {
+        public int Balance { get; set; }
+        public int Accountnumber { get; set; }
+    }
+
+}
